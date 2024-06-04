04825d0b0e07dd1e34c6072c47a47e5add6e4af5
rolandrik <rolandlepcha28@gmail.com>
2024-06-04 16:54:08 +0530

    (❁´◡`❁)

--- Django/savvy/savvyapp/static/scripts/signinSignupPopup.js
+++ Django/savvy/savvyapp/static/scripts/signinSignupPopup.js
@@ -1,7 +1,17 @@
 function openSignIn() {
-    document.getElementById("signInPopup").style.display = "block";
-  }
-  
-  function closeSignIn() {
-    document.getElementById("signInPopup").style.display = "none";
-  }
+  document.getElementById('signInPopup').style.display = "block";
+}
+
+function closeSignIn() {
+  document.getElementById('signInPopup').style.display = "none";
+}
+
+function openSignUp() {
+  document.getElementById('signInPopup').style.display = 'none';
+  document.getElementById('signUpPopup').style.display = 'block';
+}
+
+function closeSignUp() {
+  document.getElementById('signUpPopup').style.display = 'none';
+  document.getElementById('signInPopup').style.display = 'block';
+}
